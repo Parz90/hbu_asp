1ee9c85cfe576e4a1c92bab5366db3ac3900ef04
Parz <Parz90@users.noreply.github.com>
2018-01-06 19:36:11 +0100

    UserRole example implemented

---
+++ ASP/LoginRegistrationTrail/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginRegistrationTrail.Models;
+
+namespace LoginRegistrationTrail.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        //Registration POST action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified, ActivationCode")] User user)
+        {
+            return View(user);
+        }
+        //Verify Email
+
+        //Verify Email LINK
+
+        //Login 
+        
+        //Login POST
+
+        //Logout
+
+    }
+}
---
+++ ASP/RoleTrail1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoleTrail1.MvcApplication" Language="C#" %>
